dda0975394f8b4945b6a7c3ec1088934362476d4
SimoneCerri <iosonosimone@hotmail.it>
2024-02-21 16:13:01 +0100

    fix if/else positioning and fix the fizzbuzz output in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,35 @@ for ( let x=1; x<=100; x++ )
 {
     //console.log(x);
 
-//IF rest of(number / 3) = 0 print "Fizz" 
-    if ((x % 3) === 0)
+    //IF rest of(number / 5) = 0 & rest of(number / 3) = 0 print "FizzBuzz". 
+    if (((x % 5) === 0) && ((x % 3) === 0))
     {
-        console.log("fizz");
+        console.log("fizzbuzz");
     }
+    //ELSE IF rest of(number / 5) = 0 print "Buzz".
     else if ((x % 5) === 0)
     {
         console.log("buzz");
     }
-    else if (((x % 5) === 0) && ((x % 3) === 0))
+    //ELSE IF rest of(number / 3) = 0 print "Fizz".
+    else if ((x % 3) === 0) 
     {
-        console.log("fizzbuzz");
+        console.log("fizz");
     }
+    //ELSE print number.
     else
     {
         console.log(x);
     }
+
+//ELSE print number.
+
 };
 
 
-//ELSE IF rest of(number / 5) = 0 print "Buzz" too.
 
-//ELSE print number.
+
+
 
 //put those results in HTML.
 
--- readme.md
+++ readme.md
@@ -10,8 +10,9 @@ Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda ch
 
 # ?Solution? 
 - print numbers in console with FOR cycle (1-100).
-- IF rest of (number/3)=0 print "Fizz".
-- ELSE IF rest of (number/5)=0 print "Buzz" too.
+- IF rest of(number / 5) = 0 & rest of(number / 3) = 0 print "FizzBuzz".
+- ELSE IF rest of(number / 5) = 0 print "Buzz".
+- ELSE IF rest of(number / 3) = 0 print "Fizz".
 - ELSE print number.
 - put those results in HTML.
 - style those mf numbers.
