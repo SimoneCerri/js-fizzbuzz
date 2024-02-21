7a9588dea3a1a2c7ab3f3764085953f787750ae9
SimoneCerri <iosonosimone@hotmail.it>
2024-02-21 15:32:07 +0100

    add number 1 to 100 in console

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//print numbers in console with FOR cycle(1 - 100).
+for ( let x=1; x<=100; x++ )
+{
+    console.log(x);
+};
+
+
+//IF rest of(number / 3) = 0 print "Fizz".
+
+//ELSE IF rest of(number / 5) = 0 print "Buzz" too.
+
+//ELSE print number.
+
+//put those results in HTML.
+
+//style those mf numbers.
