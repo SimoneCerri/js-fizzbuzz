320d749f3f44cac9adab12282d8c6df30ea58ab0
SimoneCerri <iosonosimone@hotmail.it>
2024-02-21 15:54:36 +0100

    try to fix error but still have one problem with and operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ for ( let x=1; x<=100; x++ )
     //console.log(x);
 
 //IF rest of(number / 3) = 0 print "Fizz" 
-    if ((x % 3) = 0)
+    if ((x % 3) === 0)
     {
         console.log("fizz");
     }
-    else if ((x % 5) = 0)
+    else if ((x % 5) === 0)
     {
         console.log("buzz");
     }
-    else if ((x % 5) = 0) && ((x % 3) = 0)
+    else if ((x % 5) === 0) && ((x % 3) === 0)
     {
         console.log("fizzbuzz");
     }
