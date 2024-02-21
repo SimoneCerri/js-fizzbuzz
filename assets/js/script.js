24c6356ffe5c46769ccea2ce88c13357b32ea6fb
SimoneCerri <iosonosimone@hotmail.it>
2024-02-21 15:49:18 +0100

    try the % inside an if

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 //print numbers in console with FOR cycle(1 - 100).
 for ( let x=1; x<=100; x++ )
 {
-    console.log(x);
+    //console.log(x);
+
+//IF rest of(number / 3) = 0 print "Fizz" 
+    if ((x % 3) = 0)
+    {
+        console.log("fizz");
+    }
+    else if ((x % 5) = 0)
+    {
+        console.log("buzz");
+    }
+    else if ((x % 5) = 0) && ((x % 3) = 0)
+    {
+        console.log("fizzbuzz");
+    }
+    else
+    {
+        console.log(x);
+    }
 };
 
-
-//IF rest of(number / 3) = 0 print "Fizz".
 
 //ELSE IF rest of(number / 5) = 0 print "Buzz" too.
 
