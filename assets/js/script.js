b016e717c316105d0ac2a097f18fbf9f69cf3d7c
SimoneCerri <iosonosimone@hotmail.it>
2024-02-21 16:40:53 +0100

    add text output in html and disable console.log messagge

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,33 +6,40 @@ for ( let x=1; x<=100; x++ )
     //IF rest of(number / 5) = 0 & rest of(number / 3) = 0 print "FizzBuzz". 
     if (((x % 5) === 0) && ((x % 3) === 0))
     {
-        console.log("fizzbuzz");
+        //console.log("fizzbuzz");
+        //put those results in HTML.
+        let markup = " FizzBuzz ";
+        let gameElement = document.querySelector(".game");
+        gameElement.append(markup);
     }
     //ELSE IF rest of(number / 5) = 0 print "Buzz".
     else if ((x % 5) === 0)
     {
-        console.log("buzz");
+        //console.log("buzz");
+        //put those results in HTML.
+        let markup = " Buzz ";
+        let gameElement = document.querySelector(".game");
+        gameElement.append(markup);
     }
     //ELSE IF rest of(number / 3) = 0 print "Fizz".
     else if ((x % 3) === 0) 
     {
-        console.log("fizz");
+        //console.log("fizz");
+        //put those results in HTML.
+        let markup = " Fizz ";
+        let gameElement = document.querySelector(".game");
+        gameElement.append(markup);
     }
     //ELSE print number.
     else
     {
-        console.log(x);
+        //console.log(x);
+        //put those results in HTML.
+        let markup = ` ${x} `;
+        let gameElement = document.querySelector(".game");
+        gameElement.append(markup);
     }
-
-//ELSE print number.
-
 };
 
-
-
-
-
-
-//put those results in HTML.
 
 //style those mf numbers.
